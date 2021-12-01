6a1c015866ed0ee4971934538edce4a8c97e97fb
Lufrut <56224664+Lufrut@users.noreply.github.com>
2021-12-01 17:05:12 +0200

    add some features in adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,9 +4,9 @@ const jwt = require('jsonwebtoken')
 const ApiError = require('../error/ApiError')
 const { v4: uuidv4 } = require('uuid');
 
-const generateJWT = (id,email)=>{
+const generateJWT = (id,email,level)=>{
     return jwt.sign(
-        {id: id, email},
+        {id: id, email, level},
         process.env.SECRET_KEY,
         {expiresIn: '15d'}
     )
@@ -19,20 +19,127 @@ class adminController{
         if(!admin){
             return next(ApiError.badRequest('Invalid login or password'))
         }
-        console.log(login,admin.password)
         let comparePassword = bcrypt.compareSync(password,admin.password)
         if(!comparePassword){
             return next(ApiError.badRequest('Invalid login or password'))
         }
-        const token = generateJWT(admin.id,admin.email)
+        const token = generateJWT(admin.id,admin.email,admin.level)
         return res.json({token})
     }
-    async auth(req,res){
+    async checkUser(login,email){
+        const login_is_Exist = await User.findOne({where: {
+                login: login
+            }})
+        const email_is_Exist = await User.findOne({where: {
+                email: email
+            }})
+        if(email_is_Exist){
+            return 'User with this email already exists'
+        }
+        else if(login_is_Exist){
+            return 'User with this login already exists'
+        }
+        else{
+            return true
+        }
+    }
+    async checkUserId(id){
+        const check = await User.findOne({where: {
+            id:id
+            }})
+        if(check){
+            return 'Invalid id'
+        } else {
+            return true
+        }
+    }
+    async changeUserPassword(req,res,next){
+        const {id,password} = req.body
+        const hash_password = await bcrypt.hash(password,5)
+        const idCheck = await this.checkUserId()
+        if(idCheck===true){
+
+        } else{
+            return next(ApiError.badRequest(idCheck))
+        }
+        await User.update(
+            {
+               password:hash_password
+            },
+            {
+                where: {id: id}
+            })
+        return res.status(200).json("OK");
+    }
+    async getAllCar(req,res){
+        let {limit,page} = req.query
+        page = page || 1
+        limit = limit || 10
+        let offset = page*limit - limit
+        let cars;
+        cars = await Car.findAll({where:{limit,offset}})
+        return res.json(cars)
+    }
+    async updateUser(req,res,next){
+        const {id,login,name,lastname,phone,email,credits} = req.body
+        if(!login  || !email || !name || !lastname || !phone ||!credits){
+            return next(ApiError.badRequest('Invalid data'))
+        }
+        const check = await this.checkUser(login,email)
+        if(check===true){
+
+        } else{
+            return next(ApiError.badRequest(check))
+        }
+        const idCheck = await this.checkUserId()
+        if(idCheck===true){
+
+        } else{
+            return next(ApiError.badRequest(idCheck))
+        }
+        await User.update(
+            {
+                credits,
+                login,
+                name,
+                lastname,
+                phone,
+                email,
+            },
+            {
+                where: {id: id}
+            })
+        return res.status(200).json("OK");
 
     }
-    async getAll(req,res){
-        const admins = await Admin.findAll()
-        return res.json(admins)
+    async changeCarUserToken(req,res,next){
+        const {id,user_token} = req.body
+        if(!id||!user_token){
+            return next(ApiError.badRequest('Invalid data'))
+        }
+        await Car.update(
+            {user_token},
+            {
+                where: {id: id}
+            })
+        return res.status(200).json("OK");
+    }
+    async updateCar(req,res,next){
+        const {id,manufacturer, model, price, production_year} = req.body
+        if(!id||!manufacturer || !model || !price || !production_year ){
+            return next(ApiError.badRequest('Invalid data'))
+        }
+        await Car.update(
+            {
+                manufacturer,
+                model,
+                price,
+                production_year,
+            },
+            {
+                where: {id: id}
+            })
+        return res.status(200).json("OK");
     }
     async carRegister(req,res,next) {
         const {manufacturer, model, price, production_year} = req.body
@@ -66,17 +173,11 @@ class adminController{
             return next(ApiError.badRequest('Invalid data'))
         }
         const hash_password = await bcrypt.hash(password,5)
-        const login_is_Exist = await Admin.findOne({where: {
-                login: login
-            }})
-        const email_is_Exist = await Admin.findOne({where: {
-                email: email
-            }})
-        if(email_is_Exist){
-            return next(ApiError.badRequest('User with this email already exists'))
-        }
-        if(login_is_Exist){
-            return next(ApiError.badRequest('User with this login already exists'))
+        const check = await this.checkUser(login,email)
+        if(check===true){
+
+        } else{
+            return next(ApiError.badRequest(check))
         }
         const token = uuidv4()
         const credits = 0;
@@ -123,6 +224,5 @@ class adminController{
             })
         return res.status(200).json("OK");
     }
-
 }
 module.exports = new adminController()
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,10 +2,17 @@ const Router = require('express')
 const adminController = require("../controllers/adminController");
 const router = new Router()
 const authAdminMiddleware = require('../middleware/authAdminMiddleware')
+const checkAdminLevelMiddleware = require('../middleware/checkAdminLevelMiddleware')
 
 router.post('/login',adminController.login)
-router.post('/register',authAdminMiddleware, adminController.register)
-router.get('/auth',authAdminMiddleware, adminController.auth)
-router.get('/getAll',adminController.getAll)
+router.post('/register',checkAdminLevelMiddleware(1), adminController.register)
+router.post('/changeUserPassword',authAdminMiddleware,adminController.changeUserPassword)
+router.get('/getAllCars',authAdminMiddleware,adminController.getAllCar)
+router.post('/changeUserPassword',authAdminMiddleware,adminController.updateUser)
+router.post('/changeCarUserToken',authAdminMiddleware,adminController.changeCarUserToken)
+router.post('/updateCar',authAdminMiddleware,adminController.updateCar)
+router.post('/carRegister',authAdminMiddleware,adminController.carRegister)
+router.post('/getUser',authAdminMiddleware,adminController.getUser)
+router.post('/userRegister',authAdminMiddleware,adminController.userRegister)
 
 module.exports = router
